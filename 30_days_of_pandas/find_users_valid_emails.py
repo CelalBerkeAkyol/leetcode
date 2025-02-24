f0d1aba0ee23049eb489494d9c2659245e9453b4
CelalBerkeAkyol <buscberke@gmail.com>
2025-02-24 17:17:00 +0300

    Example 1517

---
+++ 30_days_of_pandas/find_users_valid_emails.py
@@ -0,0 +1,26 @@
+# +---------------+---------+
+# | Column Name   | Type    |
+# +---------------+---------+
+# | user_id       | int     |
+# | name          | varchar |
+# | mail          | varchar |
+# +---------------+---------+
+# user_id is the primary key (column with unique values) for this table.
+# This table contains information of the users signed up in a website. Some e-mails are invalid.
+ 
+
+# Write a solution to find the users who have valid emails.
+
+# A valid e-mail has a prefix name and a domain where:
+
+# The prefix name is a string that may contain letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'. The prefix name must start with a letter.
+# The domain is '@leetcode.com'.
+# Return the result table in any order.
+
+import pandas as pd
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    # Use the str.match() method with a regex pattern to find valid emails
+    valid_emails_df = users[users['mail'].str.match(r'^[A-Za-z][A-Za-z0-9_\.\-]*@leetcode(\?com)?\.com$')]
+    
+    return valid_emails_df
